823db21d066f37352df98befe5da3d34544e1e28
u1990105 <u1990105@campus.udg.edu>
2024-04-21 17:56:45 +0200

    canvisinicials

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,29 @@
-$('#play').on('click', 
-function(){
-    window.location.assign("./html/game.html");
-});
+addEventListener('load', function() {
+    document.getElementById('play').addEventListener('click', function(){
+        sessionStorage.removeItem("save"); // mmm mirar que es...
+        window.location.assign("./html/optionsMode.html");
+    });
 
-$('#options').on('click', 
-function(){
-    window.location.assign("./html/options.html");
-});
+    document.getElementById('saves').addEventListener('click', function(){
+        fetch("./php/load.php",{
+            method: "POST",
+            body: "",
+            headers: {"content-type":"application/json; charset=UTF-8"}
+        })
+        .then(response=>{
+            if (response.ok) response.text();
+            else trow("PHP connection fail");
+        })
+        .then(partida=>sessionStorage.save = partida)
+        .catch(err=>sessionStorage.save = localStorage.save)
+        .finally(()=>window.location.assign("./html/phasergame.html"));
+    });
 
-$('#saves').on('click', 
-function(){
-    console.error("Opció no implementada");
-});
+    document.getElementById('options').addEventListener('click', function(){
+        window.location.assign("./html/options.html");
+    });
 
-$('#exit').on('click', 
-function(){
-    console.warn("No es pot sortir!");
+    document.getElementById('exit').addEventListener('click', function(){
+        console.warn("No es pot sortir!");
+    });
 });
